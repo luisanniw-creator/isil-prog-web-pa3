168a58473e3d1196a8a43bdddc384d6190937126
luisanniw-creator <luisanniw@gmail.com>
2026-06-14 20:40:52 -0500

    Update login.php

--- login.php
+++ login.php
@@ -13,6 +13,7 @@ if (isset($_POST['ingresar'])) {
 
     $usuario = trim($_POST['usuario']);
     $password = $_POST['password'];
+    
 
     $sql = "SELECT * FROM usuario WHERE usuario = ?";
     $stmt = $conexion->prepare($sql);
